13b669f9e9d3eb3b764af6f0a089d51a71f08b79
israromar <iisraromar@gmail.com>
2023-10-13 19:42:57 +0500

    1st commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("h1", {}, "HELLO WORLD!")
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
